d1987b18abe3d9f417cb53ce59f3ca5f80dae2ae
Semnura <semanurcetiz04@icloud.com>
2023-08-08 15:00:27 +0300

    js

--- js/glide.js
+++ js/glide.js
@@ -1,40 +1,53 @@
+const productsContainer = document.getElementById("product-list");
+export function product1() {
+  const config = {
+    perView: 4,
+    gap: 20,
+    /*   autoplay: 3000, */
+    breakpoints: {
+      992: {
+        perView: 3,
+      },
+      768: {
+        perView: 2,
+      },
+      576: {
+        perView: 1,
+      },
+    },
+  };
+  productsContainer && new Glide(".product-carousel", config).mount();
+}
+const config2 = {
+  type: "carousel",
+  perView: 4,
+  gap: 20,
+  /*   autoplay: 3000, */
+  bound: true,
+  breakpoints: {
+    992: {
+      perView: 3,
+    },
+    768: {
+      perView: 2,
+    },
+    576: {
+      perView: 1,
+    },
+  },
+};
 
-const config = {
-    type: "carousel",
-    perView: 4,
-    gap: 20,
-    /*   autoplay: 3000, */
+productsContainer && new Glide(".product-carousel2", config2).mount();
+
+export function singleThumbs() {
+  const config3 = {
+    perView: 5,
     breakpoints: {
       992: {
         perView: 3,
       },
-      768: {
-        perView: 2,
-      },
-      576: {
-        perView: 1,
-      },
     },
   };
-  
-  new Glide(".product-carousel", config).mount();
-  
-  const config2 = {
-    type: "carousel",
-    perView: 4,
-    gap: 20,
-    /*   autoplay: 3000, */
-    breakpoints: {
-      992: {
-        perView: 3,
-      },
-      768: {
-        perView: 2,
-      },
-      576: {
-        perView: 1,
-      },
-    },
-  };
-  
-  new Glide(".product-carousel2", config2).mount();
+
+  new Glide(".product-thumb", config3).mount();
+}
